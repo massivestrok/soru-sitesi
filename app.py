088b12d3e681fdc208f0c9e41c36d51bcc85a867
Mehmet ali Öcalan <mehmetaliocalan@Mehmet-MBP.lan>
2025-12-31 15:27:52 +0300

    render icin temiz app.py

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     import os
+
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port)
